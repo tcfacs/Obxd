6f4cb862b9ae67054e4b2bdb7bfd8eeb10775981
Oli Larkin <olilarkin@googlemail.com>
2014-02-19 20:40:37 +0000

    commented out SSE denormal flush for the time being

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -585,8 +585,8 @@ bool ObxdAudioProcessor::getNextEvent(MidiBuffer::Iterator* iter,const int sampl
 void ObxdAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
 {
 	//SSE flags set
-	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-	_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+	//_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+	//_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 	MidiBuffer::Iterator ppp(midiMessages);
 	const ScopedLock sl (this->getCallbackLock());
 	hasMidiMessage = ppp.getNextEvent(*nextMidi,midiEventPos);
